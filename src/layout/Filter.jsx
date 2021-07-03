f53a77909dcb13ff6f1341d0617f1bde3e48274e
EnsarNar <ensarnar1@gmail.com>
2021-07-03 19:16:30 +0300

    bug fixed in rootReducer and started to Filter process

--- src/layout/Filter.jsx
+++ src/layout/Filter.jsx
@@ -4,7 +4,35 @@ import WorkTypeService from "../services/workTypeService";
 import WorkTimeService from "../services/workTimeService";
 import JobPositionService from "../services/jobPositionsService";
 import { Form } from "semantic-ui-react";
+import { useState, useEffect } from "react";
+import { useFormik } from "formik";
+import { useDispatch } from "react-redux";
+
 export default function Filter() {
+  const [cities, setCities] = useState([]);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    let cityService = new CityService();
+    cityService
+      .getCities()
+      .then((result) => setCities(result.data.data))
+      .catch((err) => console.log(err.message));
+  }, []);
+
+  const formik = useFormik({
+    initialValues: {},
+    onSubmit: (values) => {},
+  });
+  // initialValues: {
+  //     date: filters.date,
+  //     cityId: filters.cityId,
+  //     jobTypeId: filters.jobTypeId,
+  //     jobTimeId: filters.jobTimeId,
+  //     jobPositionId: filters.jobPositionId,
+  //   },
+  //   onSubmit: (values) => {
+  //     dispatch(addFilter(values));
+  //   },
   return (
     <div>
       <Form>
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import favReducer from "./reducers/favReducer";
+import filterReducer from "./reducers/filterReducer";
 
 
 const rootReducer = combineReducers({
-    favorites: favReducer
+    favorites: favReducer,
+    filterValues: filterReducer
 })
 
 
